9dabbf53c68aba252aa2121fb8c943e2f12b573e
Tim Rundström <42844873+timrundstrom@users.noreply.github.com>
2019-10-25 14:17:05 +0200

    Fist push

---
+++ Chess/Assets/Scripts/Editor/GameControllerEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameController))]
+public class GameControllerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		GameController gc = (GameController)target;
+
+		EditorGUILayout.LabelField($"White Points: {gc.whitePoints}");
+		EditorGUILayout.LabelField($"Black Points: {gc.blackPoints}");
+
+		if (GUILayout.Button("Restart Game")) {
+			gc.Restart();
+		}
+
+	}
+
+}
